563f66e34d41d105a2e52c228de6002ec6bc6610
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2019-09-30 11:30:15 -0300

    Criando cards com cursos e ajustes no header

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet"/>
     <title>E-commerce Cursos</title>
 </head>
@@ -25,6 +26,42 @@
             </ul>
         </nav>
     </header>
+    <main>
+        <section class="container mt-4">
+            <div class="row justify-content-around">
+                <div class="col-lg-3-card card text-center">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Planejamento Estratégico</h5>
+                            <img src="images/curso01.svg" class="card-img-top" alt="...">
+                            <h5>R$ 99,00</h5>
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                        </div>
+                    </div>   
+                </div>
+                <div class="col-lg-3-card card text-center">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Matemática Interespacial</h5>
+                            <img src="images/curso02.svg" class="card-img-top" alt="...">
+                            <h5>R$ 59,00</h5>
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                        </div>
+                    </div>   
+                </div>
+                <div class="col-lg-3-card card text-center">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Inovação Digital</h5>
+                            <img src="images/curso03.svg" class="card-img-top" alt="...">
+                            <h5>R$ 79,00</h5>
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                        </div>
+                    </div>   
+                </div>
+            </div>
+        </section>
+    </main>    
 
     <?php
         // $nome = "Ligia";
@@ -54,6 +91,33 @@
         
         // echo "<h1>".$usuario[0]." ".$usuario[1]." ".$usuarioDois["sobrenome"]."</h1>";
 
+        // $numero = 2;
+        // switch ($numero) {
+        //     case 1:
+        //         echo "o número é igual a 1";
+        //     break;
+        //     case 2:
+        //         echo "o número é igual a 2";
+        //     break;
+        //     default:
+        //         echo "o número é diferente de 1 e 2";
+        //     break;
+        // }
+
+        // //If Ternário
+        // $idade = 18;
+
+        // echo $idade == 16?"pré-adolescente":"Não é";
+
+        // //Testando if
+        // $idade = 16;
+
+        // if($idade < 16){
+        //     echo "Não pode votar";
+        // }
+        // elseif($idade == 16 || $idade == 17){
+        //     echo "Voto opcional";
+        // }
     ?>
 </body>
 </html>
